c8e743bb4e8a1c89d61c1a32786b429039fc39df
aeglon97 <aeglon97@gmail.com>
2019-03-24 13:26:39 +0400

    started data preprocessing

--- app/src/main/java/com/example/rakmapapp/MainActivity.java
+++ app/src/main/java/com/example/rakmapapp/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //CALCULATOR
+    float Value1, Value2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
