53b74dada7ae3d2c4ca8aea9829d1d5c082cad8c
aeglon97 <aeglon97@gmail.com>
2019-04-08 10:31:56 +0400

    implemented Google place search & place details

--- app/data/coordinates_preprocessing.py
+++ app/data/coordinates_preprocessing.py
@@ -21,22 +21,37 @@ from dotenv import load_dotenv
 #dealing with environment variables 
 path='./local.env'
 load_dotenv(dotenv_path=path, verbose=True)
-test_var = os.getenv('RAK_MAP_API_KEY')
+api_key = os.getenv('RAK_MAP_API_KEY')
 
-url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-place = input()
+findplace_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'
+findplace_inputtype = 'textquery'
+findplace_fields = 'formatted_address,name,opening_hours,geometry'
 
-res_ob = requests.get(url + 'address=' + place + '&key=' + test_var)
-x = res_ob.json()
+placedetail_url = 'https://maps.googleapis.com/maps/api/place/details/json?'
+placedetail_fields = 'formatted_address,name,opening_hours,geometry'
 
-print(x)
+findplace_results_json = requests.get(findplace_url + 'input=manar%20mall' + '&inputtype=' + findplace_inputtype +
+                              '&key=' + api_key).json()
+findplace_id = findplace_results_json['candidates'][0].get('place_id', "")
+
+#using place details to find opening hours
+placedetail_results_json = requests.get(placedetail_url + 'placeid=' + findplace_id + '&fields=' + placedetail_fields + '&key=' + api_key).json()
+
+for i in range(7):
+    print(placedetail_results_json['result']['opening_hours']['periods'][i]['open']['day'] , '\t' ,
+          placedetail_results_json['result']['opening_hours']['periods'][i]['open']['time'])
+
+
+#findplace_ob = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=falafel%20abu%20naeem&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=' + api_key)
+#res_ob = requests.get(url + 'address=' + place + '&key=' + api_key)
+#x = res_ob.json()
 
 '''
 RAK BOUNDS [lat, lng]
     Northeast: 26.0696541, 56.19553699999999
-    Southwest: 25.3543, 55.7306787
-    
+    Southwest: 25.3543, 55.7306787    
 '''
 
 #TODO 1: generate random coordinate in RAK boundary
@@ -46,6 +61,8 @@ RAK BOUNDS [lat, lng]
     #TODO 3: check if on land
 
     #TODO 4: check if nearby other businesses
+    
+    #if coordinates pass all 3 tests, continue; else generate new point & repeat
     
 #TODO 5: extract opening hours of business
     
