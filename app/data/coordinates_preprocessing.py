1e67edf024e443299b37216e958fa411e36a819a
aeglon97 <aeglon97@gmail.com>
2019-04-15 11:29:44 +0400

    added coordinates in csv

--- app/data/coordinates_preprocessing.py
+++ app/data/coordinates_preprocessing.py
@@ -4,10 +4,14 @@ Created on Sun Apr  7 08:31:52 2019
 
 @author: Angelo Acebedo
 """
+import logging
+logging.basicConfig()
+logging.basicConfig(level=logging.DEBUG)
+
 import pandas as pd
 import numpy as np
 import requests, json
-
+import geojson
 #df = pd.read_csv('Rak Businesses.csv')
 import os
 import googlemaps
@@ -19,11 +23,11 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 #dealing with environment variables 
-path='./local.env'
+'''path='./local.env'
 load_dotenv(dotenv_path=path, verbose=True)
-api_key = os.getenv('RAK_MAP_API_KEY')
+api_key = os.getenv('RAK_MAP_API_KEY')'''
 
-geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+'''geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 findplace_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'
 findplace_inputtype = 'textquery'
@@ -40,11 +44,11 @@ findplace_id = findplace_results_json['candidates'][0].get('place_id', "")
 placedetail_results_json = requests.get(placedetail_url + 'placeid=' + findplace_id + '&fields=' + placedetail_fields + '&key=' + api_key).json()
 
 print(placedetail_results_json)
-'''for i in range(7):
+for i in range(7):
     print(placedetail_results_json['result']['opening_hours']['periods'][i]['open']['day'] , '\t' ,
-          placedetail_results_json['result']['opening_hours']['periods'][i]['open']['time'])'''
+          placedetail_results_json['result']['opening_hours']['periods'][i]['open']['time'])
 
-
+'''
 #findplace_ob = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=falafel%20abu%20naeem&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=' + api_key)
 #res_ob = requests.get(url + 'address=' + place + '&key=' + api_key)
 #x = res_ob.json()
@@ -56,7 +60,10 @@ RAK BOUNDS [lat, lng]
 '''
 
 #TODO 1: generate random coordinate in RAK boundary
-    
+#shape = json.loads('C:/Users/EGA/Documents/RAK-Map/app/data/rak_rect.geojson')
+
+print(geojson.loads('rak_rect.json'))
+
     #TODO 2: check if not on road
     
     #TODO 3: check if on land
@@ -68,6 +75,3 @@ RAK BOUNDS [lat, lng]
 #TODO 5: extract opening hours of business
     
 #TODO 6: find any other interesting parameters
-
-
-
